c23bf19683a45925459cabec9b9c866ea150e54f
ARTGROUP\pshcharbakov <pawel4post@gmail.com>
2018-05-07 10:01:32 +0300

    add postgre driver

--- SQL scripts/postgre/tables.sql
+++ SQL scripts/postgre/tables.sql
@@ -1,5 +1,14 @@
-﻿CREATE TABLE IF NOT EXISTS order (
-  id_order SERIAL PRIMARY KEY,
-  client INT NOT NULL,
-  status 
-;
+﻿ALTER TABLE public.clients
+  ADD CONSTRAINT fkclient_sale FOREIGN KEY (sales_id_sale)
+      REFERENCES public.sales (id_sale) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE RESTRICT;
+	  
+ALTER TABLE public.passangers
+  ADD CONSTRAINT fk_client_pass FOREIGN KEY (id_client)
+      REFERENCES public.clients (id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE RESTRICT;
+	 
+ALTER TABLE public.trips
+  ADD CONSTRAINT fk_trip_tour FOREIGN KEY (id_tour)
+      REFERENCES public.tours_list (id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE RESTRICT;
